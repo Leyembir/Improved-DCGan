303dfb3107a18ef31ec22d6b4f90eaa0b4729283
akha0104 <akha0104@student.monash.edu>
2024-02-15 20:13:07 +1100

    added all files

---
+++ README.md
@@ -0,0 +1,96 @@
+# PyTorch Implementation of *Improved Techniques for Traning GANs*
+
+This is the extension of my [PyTorchDCGAN](https://github.com/TomatoPope0/PyTorchDCGAN) repository, adopting techniques and suggested metric from Salimans *et al.* [1]. Escpecially, I'll test my model on
+the following datasets:
+
+- MNIST [2]
+- CelebA [3], cropped
+- LSUN [4]
+- Danboru2019 [5], SFW, downscaled
+
+and compare the output with naive DCGAN [6], both qualitatively and quantitatively. However, due to the limitation on my access to the hardware, some of the planned experiments may not be performed.
+
+## Disclaimer
+
+As I'm an active learning student, this implementation may not be complete or accurate. Therefore, I recommend you to use other reliable implementations if you're willing to use it in your project.
+
+## To-Do
+
+Though I marked minibatch discrimination as implemented, it needs more optimizations. For example, in matrix subtraction, it requires a lot of memories.
+
+- [x] Feature Matching
+- [x] Minibatch Discrimination
+- [ ] Historical Averaging
+- [x] One-sided Label Smoothing
+- [ ] Virtual Batch Normalization
+
+## Model Outputs
+
+### MNIST
+
+#### Single epoch with Feature Matching
+
+![](./images/g1-fm/g1_image0.bmp)
+![](./images/g1-fm/g1_image1.bmp)
+![](./images/g1-fm/g1_image2.bmp)
+![](./images/g1-fm/g1_image3.bmp)
+![](./images/g1-fm/g1_image4.bmp)
+![](./images/g1-fm/g1_image5.bmp)
+![](./images/g1-fm/g1_image6.bmp)
+![](./images/g1-fm/g1_image7.bmp)
+![](./images/g1-fm/g1_image8.bmp)
+![](./images/g1-fm/g1_image9.bmp)
+
+#### 10 epochs with Feature Matching
+
+![](./images/g10-fm/g10_image0.bmp)
+![](./images/g10-fm/g10_image1.bmp)
+![](./images/g10-fm/g10_image2.bmp)
+![](./images/g10-fm/g10_image3.bmp)
+![](./images/g10-fm/g10_image4.bmp)
+![](./images/g10-fm/g10_image5.bmp)
+![](./images/g10-fm/g10_image6.bmp)
+![](./images/g10-fm/g10_image7.bmp)
+![](./images/g10-fm/g10_image8.bmp)
+![](./images/g10-fm/g10_image9.bmp)
+
+Unfortunately, as my vanilla DCGAN implementation did, the generator still collapsed on a small dataset of MNIST [2].
+
+#### Single epoch with Minibatch Discrimination
+
+![](./images/g1-mini/g1_image0.bmp)
+![](./images/g1-mini/g1_image1.bmp)
+![](./images/g1-mini/g1_image2.bmp)
+![](./images/g1-mini/g1_image3.bmp)
+![](./images/g1-mini/g1_image4.bmp)
+![](./images/g1-mini/g1_image5.bmp)
+![](./images/g1-mini/g1_image6.bmp)
+![](./images/g1-mini/g1_image7.bmp)
+![](./images/g1-mini/g1_image8.bmp)
+![](./images/g1-mini/g1_image9.bmp)
+
+Compared to my vanilla DCGAN implementation, images look more connected/filled, though it looks less like the actual numbers.
+
+#### 10 epochs with Minibatch Discrimination
+
+![](./images/g10-mini/g10_image0.bmp)
+![](./images/g10-mini/g10_image1.bmp)
+![](./images/g10-mini/g10_image2.bmp)
+![](./images/g10-mini/g10_image3.bmp)
+![](./images/g10-mini/g10_image4.bmp)
+![](./images/g10-mini/g10_image5.bmp)
+![](./images/g10-mini/g10_image6.bmp)
+![](./images/g10-mini/g10_image7.bmp)
+![](./images/g10-mini/g10_image8.bmp)
+![](./images/g10-mini/g10_image9.bmp)
+
+Much better! For the first time, the model doesn't collapse on long triaining!
+
+## References
+
+- [1] T. Salimans, I. Goodfellow, W. Zaremba, V. Cheung, A. Radford, and X. Chen, "Improved Techniques for Training GANs," in *NIPS 2016*, D. D. Lee, M. Sugiyama, U. V. Luxburg, I. Guyon, and R. Garnett, 2016, pp. 2234-2242.
+- [2] Y. LeCun, L. Bottou, Y. Bengio, and P. Haffner, "Gradient-based learning applied to document recognition." in *Proc. IEEE*, Nov. 1998, pp. 2278-2324.
+- [3] Z. Liu, P. Luo, X. Wang, and X. Tang, "Deep Learning Face Attributes in the Wild," in *Proc. ICCV*, Dec. 2015.
+- [4] F. Yu, Y. Zhang, S. Song, A. Seff, and J. Xiao, "LSUN: Construction of a Large-scale Image Dataset using Deep Learning with Humans in the Loop," 2015, *arXiv:1506.03365*.
+- [5] Anonymous, and Danbooru Community, and G. Branwen, Jan. 2020, "Danbooru2019: A Large-Scale Crowdsourced and Tagged Anime Illustration Dataset," Web. [Online]. Available: https://www.gwern.net/Danbooru2019.
+- [6] A. Radfold, L. Metz, and S. Chintala, "Unsupervised Representation Learning with Deep Convolutional Generative Adversarial Networks," 2015, *arXiv:1511.06434*.
---
+++ train.py
@@ -0,0 +1,133 @@
+import argparse
+import pickle
+import torch
+import torchvision
+import torchvision.transforms as T
+from dcgan import Generator, Discriminator
+
+parser = argparse.ArgumentParser("Hyperparameters and Dataset Arguments")
+parser.add_argument("--dataset", "-ds", type=str, default="mnist")
+parser.add_argument("--batch-size", "-bs", type=int, default=64)
+parser.add_argument("--num-noises", "-n", type=int, default=100)
+parser.add_argument("--depths", "-d", type=int, default=128)
+parser.add_argument("--learning-rate", "-lr", type=float, default=0.0002)
+parser.add_argument("--alpha", "-sl", type=float, default=0.9)
+parser.add_argument("--beta-1", "-b1", type=float, default=0.5)
+parser.add_argument("--beta-2", "-b2", type=float, default=0.99)
+parser.add_argument("--epochs", "-e", type=int, default=10)
+parser.add_argument("--report", "-r", type=int, default=100)
+parser.add_argument("--workers", "-w", type=int, default=4)
+# If feature matching is not used, minibatch discrimination is used instead
+parser.add_argument("--feature-matching", "-fm", type=bool, default=False)
+args = parser.parse_args()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+if args.dataset == "mnist":
+    IMAGE_SIZE = 32
+    NUM_COLORS = 1
+    data = torchvision.datasets.ImageFolder(root="art_dataset", transform = T.Compose([
+        T.Resize(IMAGE_SIZE),  # Resize the shortest side to IMAGE_SIZE
+        T.CenterCrop(IMAGE_SIZE),  # Crop the center IMAGE_SIZE x IMAGE_SIZE
+        T.ToTensor(),
+        T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Mean and std for 3 channels
+    ]))
+
+    
+    dataloader = torch.utils.data.DataLoader(
+        data,
+        batch_size=192,
+        shuffle=True,
+        num_workers=args.workers,
+        drop_last=True
+    )
+else:
+    raise Exception("Not a valid dataset")
+
+G = Generator(args.num_noises, NUM_COLORS, args.depths, IMAGE_SIZE).to(device)
+D = Discriminator(NUM_COLORS, args.depths, IMAGE_SIZE).to(device)
+
+def init_weight(model):
+    classname = model.__class__.__name__
+    if classname.find('conv') != -1:
+        torch.nn.init.normal_(model.weight.data, 0, 0.02)
+
+G.apply(init_weight)
+D.apply(init_weight)
+
+criterion_d = torch.nn.BCELoss()
+criterion_g = torch.nn.MSELoss() if args.feature_matching else torch.nn.BCELoss()
+optimizer_g = torch.optim.Adam(
+    G.parameters(),
+    lr=args.learning_rate,
+    betas=[args.beta_1, args.beta_2]
+)
+optimizer_d = torch.optim.Adam(
+    D.parameters(),
+    lr=args.learning_rate,
+    betas=[args.beta_1, args.beta_2]
+)
+
+if __name__ == "__main__":
+    # One-sided label smoothing
+    pos_labels = torch.full((args.batch_size, 1), args.alpha, device=device)
+    neg_labels = torch.zeros((args.batch_size, 1), device=device)
+
+    for epoch in range(args.epochs):
+        losses_d, losses_g = [], []
+        for i, data in enumerate(dataloader):
+            # Train D with genuine data
+            genuine = data[0].to(device) # Drop label data
+            genuine = genuine.reshape(-1, NUM_COLORS, IMAGE_SIZE, IMAGE_SIZE)
+
+            optimizer_d.zero_grad()
+
+            output = D(genuine)
+            
+            loss_d_geunine = criterion_d(output, pos_labels)
+            loss_d_geunine.backward()
+
+            # Train D with fake data
+            noise = torch.FloatTensor(args.batch_size, args.num_noises).uniform_(-1, 1).to(device)
+            fake = G(noise)
+
+            output = D(fake.detach())
+            loss_d_fake = criterion_d(output, neg_labels)
+            loss_d_fake.backward()
+
+            optimizer_d.step()
+
+            # Train G with fake data
+            optimizer_g.zero_grad()
+
+            # Feature matching
+            if args.feature_matching:
+                target = D.conv[:-3](genuine)
+                output = D.conv[:-3](fake)
+                loss_g = criterion_g(output, target)
+                loss_g.backward()
+            # Minibatch discrimination
+            else:
+                output = D(fake)
+                loss_g = criterion_g(output, pos_labels)
+                loss_g.backward()
+
+            optimizer_g.step()
+
+            # Report & record loss
+            if i % args.report == args.report-1:
+                print("[%d/%d][%d/%d] D:%.7f G:%.7f" %
+                    (epoch, args.epochs, i, len(dataloader), loss_d.item(), loss_g.item()))
+
+            loss_d = loss_d_geunine + loss_d_fake
+            losses_d.append(loss_d.item())
+            losses_g.append(loss_g.item())
+
+    # Save generator model
+    torch.save(G.state_dict(), "./models/g%d.pt" % args.epochs)
+
+    # Save loss records
+    with open("./metrics/loss_g%d.pkl" % args.epochs, "wb") as g:
+        pickle.dump(losses_g, g)
+    with open("./metrics/loss_d%d.pkl" % args.epochs, "wb") as d:
+        pickle.dump(losses_d, d)
